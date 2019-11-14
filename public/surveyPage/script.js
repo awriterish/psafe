a35f926bd69e2f43fb4d83d264dbfdff20062347
samoreGE <samoreGE@hendrix.edu>
2019-11-14 00:56:59 -0600

    Most of it works now!

--- public/surveyPage/script.js
+++ public/surveyPage/script.js
@@ -1,15 +1,20 @@
 //TEMP VARIABLES (REPLACE W/ SQL WHEN POSSIBLE)
 const totalStudents=10;
-const tempOutcomesArray = ['This is test outcome 1!', 'This is another outcome! (it\'s the second one)', '3rd outcome is the charm!', 'Ok, maybe we add another outcome here at the end.'];
+const tempOutcomesArray = [
+['CSCI 340', 'This is test outcome 1!', 'This is another outcome! (it\'s the second one)', '3rd outcome is the charm!', 'Ok, maybe we add another outcome here at the end.'],
+['TART 140', 'This is an alternate outcome', 'This is another alternate! (it\'s the second one)', 'Let\'s stop this one at 3!']];
 //COLOR SCHEME STUFF
 var goodColor="#98FB98";
 var badColor="#F08080";
 //LOCAL VARIABLES
+var selectedClass;
 var validToSubmit=false;
 var totalOutcomes=0;
 
 //ON RUNTIME
-setUpPage(tempOutcomesArray);
+makeDropdownList();
+makeSurvey();
+makeClassOutcomes(0,tempOutcomesArray)
 
 //REACTION FUNCTIONS
 $('.number-box').keyup(function () { 
@@ -20,7 +25,60 @@ $('.number-box').on("change", function () {
 });
 
 //MAIN FUNCTIONS
-function setUpPage(outcomeArray) {
+function makeDropdownList() {
+	console.log("makeDropdownList()");
+	var newOutcome = $("<div class=\'dropdown-section\'>");
+	newOutcome.html("\
+		<h1>\
+				Which class would you like to fill a survey for?\
+			</h1>\
+			<div class=\"dropdown\">\
+				<button class=\"dropbtn\">Classes</button>\
+				<div class=\"dropdown-content\">\
+					<a href=\"#\">Link 1 asdfasdfasdf</a>\
+					<a href=\"#\">Link 2</a>\
+					<a href=\"#\">Link 3</a>\
+				</div>\
+			</div>");
+	$("#dropdown-container").append(newOutcome);
+}
+
+function makeSurvey() {
+	console.log("makeSurvey()");
+	var newOutcome = $("<div id=\'survey\'>");
+	newOutcome.html("\
+		<table class='table outcome-table'>\
+				<thead>\
+					<tr class = 'names-row'>\
+						<th scope='col' class='top-col'>Outcome</th>\
+						<th scope='col' class='top-col'>STR</th>\
+						<th scope='col' class='top-col'>SAT</th>\
+						<th scope='col' class='top-col'>NG</th>\
+						<th scope='col' class='top-col'>UNSAT</th>\
+						<th scope='col' class='top-col'>NA</th>\
+						<th scope='col' class='top-col'>SUM</th>\
+					</tr>\
+				</thead>\
+				<tbody id='outcomes'>\
+				</tbody>\
+			</table>\
+			<div class='button-row'>\
+				<button type='button' id='submit-button'>Submit</button>\
+			</div>");
+	$("#survey-container").append(newOutcome);
+}
+
+function makeClassOutcomes(surveyIndex, outcomeArray) {
+	console.log("makeClassOutcomes("+surveyIndex+")");
+	var questionsArray=[];
+	for (i = 1; i < outcomeArray[surveyIndex].length; i++) {
+		console.log("i="+i);
+		console.log("outcomeArray[surveyIndex]["+i+"]="+outcomeArray[surveyIndex][i]);
+		questionsArray.push(outcomeArray[surveyIndex][i]);
+	}
+	setUpSurvey(questionsArray);
+}
+function setUpSurvey(outcomeArray) {
 	console.log("setUpPage("+outcomeArray+")");
 	addOutcomesFromArray(outcomeArray);
 	refreshAll();
@@ -93,7 +151,7 @@ function refreshSubmitButton(sumsMatched) {
 	}
 }
 function refreshValidToSubmit() {
-	console.log("refreshSums()");
+	console.log("refreshValidToSubmit()");
 	validToSubmit=allSumsMatched();
 }
 function allSumsMatched() {
@@ -110,17 +168,17 @@ function allSumsMatched() {
 	return matched;
 }
 function getSumOf(num) {
-	console.log("getSumOf("+num+")");
+//	console.log("getSumOf("+num+")");
 	var str=Number($("#str"+num).val());
-	console.log("str"+num+"="+str);
+//	console.log("str"+num+"="+str);
 	var sat=Number($("#sat"+num).val());
-	console.log("sat"+num+"="+sat);
+//	console.log("sat"+num+"="+sat);
 	var ng=Number($("#ng"+num).val());
-	console.log("ng"+num+"="+ng);
+//	console.log("ng"+num+"="+ng);
 	var unsat=Number($("#unsat"+num).val());
-	console.log("unsat"+num+"="+unsat);
+//	console.log("unsat"+num+"="+unsat);
 	var na=Number($("#na"+num).val());
-	console.log("na"+num+"="+na);	
+//	console.log("na"+num+"="+na);	
 	var sum=Number(str+sat+ng+unsat+na);
 	return sum;
 }
