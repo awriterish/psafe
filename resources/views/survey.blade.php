787d727bdd6d81a676b43e8c99234be684d14ab2
samoreGE <samoreGE@hendrix.edu>
2019-11-23 17:53:15 -0600

    Update survey.blade.php

--- resources/views/survey.blade.php
+++ resources/views/survey.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-	<script src="{{ URL::asset('js/surveyScript.js') }}"></script>		
+			
 	<div class="container" id="dropdown-container">
 			<div class="row" id="dropdown-row">
 		</div>
@@ -9,5 +9,6 @@
 	<div class="container" id='survey-container' onLoad="helloWorld({{$id}})">
 	</div>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js "></script>
+	<script src="{{ URL::asset('js/surveyScript.js') }}"></script>
 	
 @endsection
