6ba81d493f480a509dadc5d30fb26ef41c69feca
samoreGE <samoreGE@hendrix.edu>
2019-11-23 16:57:47 -0600

    trying to transfer survey page to actual SQL

--- resources/views/survey.blade.php
+++ resources/views/survey.blade.php
@@ -1,5 +1,12 @@
 @extends('layout')
 
 @section('content')
-
+	<div class="container" id="dropdown-container">
+			<div class="row" id="dropdown-row">
+		</div>
+	</div>
+	<div class="container" id='survey-container'>
+	</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js "></script>
+	<script src="surveyScript.js"></script>
 @endsection
