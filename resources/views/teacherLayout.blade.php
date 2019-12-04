d8898b86c91ec4d86839bf6eb94b4d6a96dc6658
adsfdf <earnestia@hendrix.edu>
2019-12-03 21:26:28 -0600

    again

--- resources/views/teacherLayout.blade.php
+++ resources/views/teacherLayout.blade.php
@@ -112,7 +112,7 @@ then ID that says which survey is active, call
 
           <canvas class="my-4" id="myChart" width="900" height="380"></canvas>
 
-          <h2>@yield('contentTitle','CONTENTTITLE GOES HERE :)')</h2>
+          <h2>@yield('contentTitle','CONTENTTITLE GOES HERE c:')</h2>
           @yield('content','CONTENT GOES HERE c:')
         </main>
       </div>
