968d3454aca8039184ecc0278df5baaadea50646
samoreGE <samoreGE@hendrix.edu>
2019-12-03 20:49:18 -0600

    Update SurveyController.php

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -10,22 +10,50 @@ class SurveyController extends Controller
 	
     public function survey($id) {
 		$rowNum=0;
-		$outStdClass = json_decode ("{}");
+		$outJSON = json_encode(json_decode ("{}"));
 		
 		$idMatch1 = DB::table('Class')
 						->select('Class.Name as ClassName', 'Class.Class_ID',
 								'Learning Domains.Name as DomainName', 'Learning Domains.Domain_ID')
 						->join('Learning Domains', 'Class.Domain_ID', '=', 'Learning Domains.Domain_ID')
+						->where('Learning Domains.Active', 1)
+						->where('Teacher_ID', $id)
+						->orWhere('Teacher_ID2', $id)
+						->get();
+
+		$idMatch2 = DB::table('Class')
+						->select('Class.Name as ClassName', 'Class.Class_ID',
+								'Learning Domains.Name as DomainName', 'Learning Domains.Domain_ID')
+						->join('Learning Domains', 'Class.Domain_ID2', '=', 'Learning Domains.Domain_ID')
+						->where('Learning Domains.Active', 1)
 						->where('Teacher_ID', $id)
 						->orWhere('Teacher_ID2', $id)
 						->get();
 		
-		$outJSON = json_encode($idMatch1);
+		$idMatch3 = DB::table('Class')
+						->select('Class.Name as ClassName', 'Class.Class_ID',
+								'Learning Domains.Name as DomainName', 'Learning Domains.Domain_ID')
+						->join('Learning Domains', 'Class.Domain_ID3', '=', 'Learning Domains.Domain_ID')
+						->where('Learning Domains.Active', 1)
+						->where('Teacher_ID', $id)
+						->orWhere('Teacher_ID2', $id)
+						->get();
 		
-		dd($outJSON);
+		$match1Encoded=json_encode($idMatch1);
+		$match2Encoded=json_encode($idMatch2);
+		$match3Encoded=json_encode($idMatch3);
+		
+		print_r($match1Encoded);
+		print_r($match2Encoded);
+		print_r($match3Encoded);
+		
+		$Merge1and2 = json_encode(array_merge(json_decode($match1Encoded, true),json_decode($match2Encoded, true)));
+		$ClassesToSurvey = json_encode(array_merge(json_decode($Merge1and2, true),json_decode($match3Encoded, true)));
+
+		dd($ClassesToSurvey);
 
 		$data=array('id'=>$id,
-					'outJSON'=>$outJSON);
+					'ClassesToSurvey'=>$ClassesToSurvey);
 		return view('survey')->with($data);
 	}
 }
