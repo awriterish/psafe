dcf480a9ecc6e2c78a6881a1298b55590611d4a7
samoreGE <samoreGE@hendrix.edu>
2019-12-05 13:08:31 -0600

    Setting up methods for Isaac's stuff

--- public/js/surveyUI.js
+++ public/js/surveyUI.js
@@ -2,6 +2,31 @@
 //Class Name, Class ID, Domain Abbr, Domain ID, Students
 var surveys = [];
 
+//Domain ID, Question ID, Question Text
+var questions = [];
+
+var teacherID = -1;
+
+//HELPER METHODS
+function getSurveyTitles() {
+	return ["test1", "test2", "test3"];
+}
+
+function getSurveyQuestions(index) {
+	return [index+"question1", index+"question2", index+"question3"];
+}
+
+
+/*
+[[class index],
+[STR, SAT, NG, UNSAT, NA],
+...,
+[Grades?, Papers?, Presentations?, Exams?, Other?(String)]
+*/
+function parseSubmission(submission) {
+	console.log("parseSubmission("+submission+")");
+}
+
 
 function addNewSurvey(className, classID, domainName, domainID, students) {
 	console.log('addNewSurvey('+className+', '+classID+', '+domainName+', '+domainID+', '+students+')');
