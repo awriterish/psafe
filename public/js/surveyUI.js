d0dd562d3d1be574518378cfead8a0715128155e
samoreGE <samoreGE@hendrix.edu>
2019-12-05 13:12:12 -0600

    Update surveyUI.js

--- public/js/surveyUI.js
+++ public/js/surveyUI.js
@@ -8,14 +8,21 @@ var questions = [];
 var teacherID = -1;
 
 //HELPER METHODS
+/*
+Returns the titles of all surveys in the following format:
+[ClassName1/DomainAbbr1, ClassName2/DomainAbbr2, ClassName3/DomainAbbr3,...]	
+*/
 function getSurveyTitles() {
-	return ["test1", "test2", "test3"];
+	return ["test1/LD1", "test2/LD2", "test3/LD3"];
 }
 
+/*
+Returns all the questions of a survey at {index} in the following format:
+[Question 1, Question 2, Question 3,...]	
+*/
 function getSurveyQuestions(index) {
 	return [index+"question1", index+"question2", index+"question3"];
 }
-
 
 /*
 [[class index],
