87e298455b1e8d3e83fe66b0b1f95f4c75712593
samoreGE <samoreGE@hendrix.edu>
2019-12-04 20:07:21 -0600

    Update SurveyController.php

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -9,8 +9,8 @@ class SurveyController extends Controller
 {
 	
     public function survey($id) {
-		$emptyArray = [];
 		
+		//Class Queries
 		$idMatch1 = DB::table('Classes')
 						->select('Classes.Course Code as Class_Name', 'Classes.Class_ID',
 								'Learning Domains.Abbr as Domain_Name', 'Learning Domains.Domain_ID', 'Classes.Num_Students')
@@ -37,21 +37,33 @@ class SurveyController extends Controller
 						->orWhere('Teacher_ID2', $id)
 						->get();
 		
+		//Class Query Encoding/Merging
 		$match1Encoded=json_encode($idMatch1);
 		$match2Encoded=json_encode($idMatch2);
 		$match3Encoded=json_encode($idMatch3);
-		
 		$Merge1and2 = json_encode(array_merge(json_decode($match1Encoded, true),json_decode($match2Encoded, true)));
 		$ClassesToSurvey = array_merge(json_decode($Merge1and2, true),json_decode($match3Encoded, true));
 		
+		//Possible Question Query [code, text, domain ID]
+		$questionIDs = [];
 		forEach($ClassesToSurvey as $result) {
+			$domainID=$result['Domain_ID'];
+			if(in_array($domainID, $questionIDs)) {
+				
+			} else {
+				array_push($questionIDs, $domainID);
+			}
+		}
+		
+		forEach($questionIDs as $ID) {
 			
 		}
 		
 		//dd($ClassesToSurvey);
 		
 		$data=array('id'=>$id,
-					'ClassesToSurvey'=>$ClassesToSurvey);
+					'ClassesToSurvey'=>$ClassesToSurvey,
+					'questionIDs'=>$questionIDs);
 		return view('survey')->with($data);
 	}
 }
