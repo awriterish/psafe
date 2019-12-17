df8e7624c3cec62d93b6d385ace2ea3777c63fa2
Nova Blankenship <awriterish@outlook.com>
2019-12-16 22:11:23 -0600

    Added account verification
    This file verifies accounts upon login.

--- app/Http/Controllers/Login.php
+++ app/Http/Controllers/Login.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Socialite;
+use Illuminate\Support\Facades\DB;
 
 class Login extends Controller
 {
@@ -12,5 +13,12 @@ class Login extends Controller
       $name = $user->user["givenName"] . " " . $user->user["surname"];
       $hash = hash('sha256',$name);
       $key = $user->id;
+      $teacher = DB::table('Teachers')
+        ->select("*")
+        ->where("Teacher_ID",$hash)
+        ->get();
+      if($teacher->isEmpty()){
+        return view('noAccount');
+      }
     }
 }
---
+++ resources/views/noAccount.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>No Account</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div class="container">
+      <h3 class="display-2 text-center" style="border-bottom: 2px solid grey; margin-top: 35%">Account Restricted</h3>
+      <p class="text-center">Uh-oh! It looks like you are not allowed to access this resource<br>If you believe this is an error, please tell the owner of this site to upadte the database.</p>
+    </div>
+  </body>
+</html>
